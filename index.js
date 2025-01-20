847915181583f0c2e145086f687fcbfe930b8957
SandaniDhammage <sandanid01@gmail.com>
2025-01-21 03:33:27 +0530

    commit1

---
+++ index.js
@@ -0,0 +1,28 @@
+function search() {
+    let inputTag = document.getElementById("searchbox");
+    let movieName = inputTag.value; 
+    console.log(movieName);
+    
+    let htmlRequest = new XMLHttpRequest();
+    let url = "http://www.omdbapi.com/?apikey=3651fa9f&t=" + encodeURIComponent(movieName); 
+    htmlRequest.open("GET", url);
+    htmlRequest.responseType = "json";
+    htmlRequest.onload = () => {
+        let response = htmlRequest.response;
+        console.log(response); 
+        
+        if(response.Response == "True") {
+            
+            localStorage.setItem('movieDetails', JSON.stringify(response));
+            
+         
+            window.location.href = 'movieDetails.html'; 
+        } else {
+            console.log("No movies found");
+            alert("Movie not found!");
+        }
+    };
+
+    htmlRequest.send();
+}
+
